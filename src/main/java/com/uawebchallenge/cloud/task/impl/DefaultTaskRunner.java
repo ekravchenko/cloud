3f9bebc12b3a4db3bfb98a464d4bf57cd089544c
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-04-28 00:59:27 +0300

    Started implementing worker

---
+++ src/main/java/com/uawebchallenge/cloud/task/impl/DefaultTaskRunner.java
@@ -0,0 +1,12 @@
+package com.uawebchallenge.cloud.task.impl;
+
+import com.uawebchallenge.cloud.task.Task;
+import com.uawebchallenge.cloud.task.TaskRunner;
+
+public class DefaultTaskRunner implements TaskRunner {
+
+    @Override
+    public void run(Task task) {
+
+    }
+}
--- src/main/java/com/uawebchallenge/cloud/worker/Worker.java
+++ src/main/java/com/uawebchallenge/cloud/worker/Worker.java
@@ -1,14 +1,58 @@
 package com.uawebchallenge.cloud.worker;
 
-public class Worker extends Thread {
+import com.uawebchallenge.cloud.store.Store;
+import com.uawebchallenge.cloud.task.Task;
+import com.uawebchallenge.cloud.task.TaskManager;
+import com.uawebchallenge.cloud.task.TaskRunner;
+import com.uawebchallenge.cloud.task.exception.TaskException;
+import com.uawebchallenge.cloud.task.impl.DefaultTaskManager;
+import com.uawebchallenge.cloud.task.impl.DefaultTaskRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    @Override
-    public void run() {
-        // TODO See list below...
-        // get task from pool
-        // If I could get task - execute it
-        // if task was executed - reset WorkerSleep
-        // if task was not found - increase WorkerSleep
-        // Sleep )
+import java.util.Optional;
+
+public class Worker {
+
+    private final static Logger logger = LoggerFactory.getLogger(Worker.class);
+    private final TaskManager taskManager;
+    private final TaskRunner taskRunner;
+    private final WorkerSleep workerSleep;
+    private boolean stop;
+
+    public Worker(Store store) {
+        this.taskManager = new DefaultTaskManager(store);
+        this.taskRunner = new DefaultTaskRunner();
+        this.workerSleep = new WorkerSleep();
+    }
+
+    public void stop(boolean stop) {
+        this.stop = stop;
+    }
+
+    public void work() {
+        while (!stop) {
+            try {
+                Optional<Task> taskOptional = taskManager.nextPendingTask();
+                if (taskOptional.isPresent()) {
+                    Task task = taskOptional.get();
+                    taskRunner.run(task);
+                    workerSleep.reset();
+                } else {
+                    sleepQuietly(workerSleep.getSleep());
+                    workerSleep.increase();
+                }
+            } catch (TaskException e) {
+                logger.error(e.getMessage());
+            }
+        }
+    }
+
+    private void sleepQuietly(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            logger.error("Unexpected InterruptedException when freezing thread.", e);
+        }
     }
 }
