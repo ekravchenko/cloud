45c2cea46d2b8bfa3b8471647c989a77125c97ef
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-04-28 08:43:43 +0300

    Fixed wanrnings

--- src/test/java/com/uawebchallenge/cloud/script/DefaultScriptRunnerTest.java
+++ src/test/java/com/uawebchallenge/cloud/script/DefaultScriptRunnerTest.java
@@ -1,5 +1,6 @@
 package com.uawebchallenge.cloud.script;
 
+import com.uawebchallenge.cloud.exception.TaskException;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -14,14 +15,14 @@ public class DefaultScriptRunnerTest {
     private final ScriptCloudGateway mockCloudGateway = Mockito.mock(ScriptCloudGateway.class);
 
     @Test
-    public void runWithCloudGateway() throws ScriptException {
+    public void runWithCloudGateway() throws ScriptException, TaskException {
         String script = "var task={input: 5, script: 'function(input) {return input + 1;}'};" +
                 "cloud.createTask(task);";
 
         ScriptRunner scriptRunner = new DefaultScriptRunner(mockCloudGateway);
         scriptRunner.run(script);
 
-        Map<String, Object> expectedValues = new HashMap<String, Object>();
+        Map<String, Object> expectedValues = new HashMap<>();
         expectedValues.put("input", 5);
         expectedValues.put("script", "function(input) {return input + 1;}");
         verify(mockCloudGateway).createTask(ScriptObjectMirrorMatcher.matchesValues(expectedValues));
