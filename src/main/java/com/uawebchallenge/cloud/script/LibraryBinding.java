30b393ffd7da26725cc131ce86921975a311c379
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-04-29 17:56:49 +0300

    Added DataException

--- src/main/java/com/uawebchallenge/cloud/script/LibraryBinding.java
+++ src/main/java/com/uawebchallenge/cloud/script/LibraryBinding.java
@@ -1,12 +1,13 @@
 package com.uawebchallenge.cloud.script;
 
+import com.uawebchallenge.cloud.exception.DataException;
 import com.uawebchallenge.cloud.exception.ScriptException;
 
 import javax.script.Bindings;
 
 interface LibraryBinding {
 
-    void export(String libraryName, Bindings script);
+    void export(String libraryName, Bindings script) throws DataException;
 
-    void require(String libraryName) throws ScriptException;
+    void require(String libraryName) throws ScriptException, DataException;
 }
