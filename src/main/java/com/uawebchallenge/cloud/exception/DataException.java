9db7c713c9647d7b19d46f437de06446b566fac6
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-04-29 17:59:46 +0300

    Added DataException

---
+++ src/main/java/com/uawebchallenge/cloud/exception/DataException.java
@@ -0,0 +1,24 @@
+package com.uawebchallenge.cloud.exception;
+
+public class DataException extends Exception {
+
+    private DataException(String message) {
+        super(message);
+    }
+
+    public static DataException serializationError(Object object, String details) {
+        String error = "Couldn't serialize object " + object + ". Error details: " + details;
+        return new DataException(error);
+    }
+
+    public static DataException deserializationError(String details) {
+        String error = "Couldn't deserialize object that was received. Error details: " + details;
+        return new DataException(error);
+    }
+
+    public static DataException classNotFoundError(String details) {
+        String error = "Couldn't deserialize object that was received. Specific class was not found. Error details: " + details;
+        return new DataException(error);
+    }
+
+}
